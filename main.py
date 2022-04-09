f6b0d57619e28b24d56e80862d6e0fa55fff342a
ali-karimi-git <96233443+ali-karimi-git@users.noreply.github.com>
2022-04-09 13:03:27 +0430

    Update main.py
    add error and a loop for guess word len

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from operator import le
 import sys
 
-myword = "ali"
-yourword = input()
+myword = "kali"
+yourword = input("guess a word")
+
+while len(myword) != len(yourword):
+    yourword = input("barabar nistan, try another word")
+
 
 if myword == yourword:
     print ("you did it")
@@ -12,20 +17,20 @@ yourword_list = list(yourword)
 
 print (myword_list)
 
+if len(myword_list) != len(yourword_list):
+    print("ham andaze nistan")
+    sys.exit()
 
 el = 0
 b = 0
 
 guess_list = []
-guess_list2 = []
-
 
 for i in yourword_list:
     if yourword_list[b] == myword_list[el]:
         print("exist " + myword_list[el] + " " ,str(el + 1))
     elif yourword_list[b] != myword_list[el]:
-        guess_list2.append(yourword_list[b])
-        #print("doesnt exist " + yourword_list[b] + " " ,str(el + 1))
+        print("doesnt exist " + yourword_list[b] + " " ,str(el + 1))
         guess_list.append(yourword_list[b])
     b = b + 1
     el = el + 1
@@ -33,5 +38,3 @@ for i in yourword_list:
 for i in yourword_list:
     if i in guess_list:
         print ("hastan vali sare jashoon nistan " +i)
-
-
